2236772b34b491bd5ca3e8fbb7ea1deeac156ac7
Stu Dev <srinikesh.lidolearning.12@gmail.com>
2021-07-18 16:38:18 +0530

    adding-some-css

--- js/SN.js
+++ js/SN.js
@@ -1,57 +1,19 @@
 var greeting = document.querySelector("#greeting");
 var time = document.querySelector("#time");
+var result = 0;
 
-// var choice;
-// if (choice==="%"){
-//       num1=num1ch.value;
-//       ans1.innerHTML="Here is your Answer!!";
-//       document.getElementById("Answer1").innerHTML=percentage(num1);
-//       for (i=1;i<41;i=i+1){
-//               document.getElementById(i).innerHTML="";
-//       }
-//     }
+function Circle(str) {
+ num1=Number(num1ch.value);
+ const pi=3.141592653589793238462;
+ result=pi*num1*num1;
+ return result;
+}
 
-// else if(choice==="//") {
-//         num1=num1ch.value;
-//         ans1.innerHTML="Here is your Answer!!";
-//         document.getElementById("Answer1").innerHTML=sq_rt(num1);
-//         for (i=1;i<41;i=i+1){
-//                 document.getElementById(i).innerHTML="";
-//         }
-// }else if (choice==="***"){
-//         ans1.innerHTML="";
-//         document.getElementById("Answer1").innerHTML="";
-
-//         //location.replace("tables.html");
-//         num1=Number(num1ch.value);
-//         ans1="Here is your" + " " + num1 + " " + "Table!!";
-
-//         for (i=1;i<51;i=i+1){
-//                 n=i;
-//                 m=num1*i;
-//                 document.getElementById("Answer1").innerHTML='Here is your'+ ' ' + num1 +' '+'Tables';
-//                 document.getElementById(i).innerHTML=num1 + " " + "x" +" " + i +" " + "=" + "  " + m ;
-//         }
-// }else if (choice==="****"){
-//         num1=Number(num1ch.value);
-//         ans1.innerHTML="Here is your Answer!!";
-//         document.getElementById("Answer1").innerHTML=Circle(num1);
-
-// }
-// function Circle(str){
-//  result=" ";
-//  num1=Number(num1ch.value);
-//  const pi=3.141592653589793238462;
-//  m=pi*num1*num1;
-//  return m;
-// }
-
-// function sq_rt(str){
-//   result=" ";
-//   num1=Number(num1ch.value);
-//   result=Math.sqrt(num1);
-//   return result;
-// }
+function sq_rt(str){
+  num1=Number(num1ch.value);
+  result=Math.sqrt(num1);
+  return result;
+}
 
 function displayTime(){
         var date= new Date();
